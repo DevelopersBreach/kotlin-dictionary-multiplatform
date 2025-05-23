812186fa57af73b916171e3335785111700bdc79
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-05-23 20:07:15 +0530

    Revert changes to main.kt

--- composeApp/src/desktopMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/main.kt
+++ composeApp/src/desktopMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/main.kt
@@ -2,21 +2,12 @@ package com.developersbreach.kotlindictionarymultiplatform
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import com.developersbreach.kotlindictionarymultiplatform.di.appModule
-import com.developersbreach.kotlindictionarymultiplatform.ui.navigation.AppNavigation
-import com.developersbreach.kotlindictionarymultiplatform.ui.screens.topic.TopicScreen
-import org.koin.core.context.startKoin
 
-fun main() {
-    startKoin {
-        modules(appModule)
-    }
-    application {
-        Window(
-            onCloseRequest = ::exitApplication,
-            title = "Kotlin Dictionary Multiplatform",
-        ) {
-            AppNavigation()
-        }
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Kotlin Dictionary Multiplatform",
+    ) {
+        App()
     }
 }
