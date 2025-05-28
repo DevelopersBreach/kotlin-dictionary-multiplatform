f84096f92fe75d2e5a98cff5dd33459aa0e36bed
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-05-28 11:28:23 +0530

    Run `ktlint` format

--- composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/Log.kt
+++ composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/Log.kt
@@ -2,21 +2,33 @@ package com.developersbreach.kotlindictionarymultiplatform
 
 import co.touchlab.kermit.Logger
 
-
 object Log {
-    fun d(tag: String, message: String) {
+    fun d(
+        tag: String,
+        message: String,
+    ) {
         Logger.d(message, tag = tag)
     }
 
-    fun i(tag: String, message: String) {
+    fun i(
+        tag: String,
+        message: String,
+    ) {
         Logger.i(message, tag = tag)
     }
 
-    fun w(tag: String, message: String) {
+    fun w(
+        tag: String,
+        message: String,
+    ) {
         Logger.w(message, tag = tag)
     }
 
-    fun e(tag: String, message: String, throwable: Throwable? = null) {
+    fun e(
+        tag: String,
+        message: String,
+        throwable: Throwable? = null,
+    ) {
         Logger.e(message, throwable, tag = tag)
     }
 }
