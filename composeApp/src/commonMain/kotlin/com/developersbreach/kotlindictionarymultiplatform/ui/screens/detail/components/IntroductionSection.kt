92e76310d2bf55ad716d11644e72fe6d9a145f4a
YugaJ7 <jaiswal.yuga7@gmail.com>
2025-06-03 23:18:20 +0530

    - Create modular composables for detail screen sections. - Add `IntroductionSection` composable. - Add `TableOfContents` composable. - Add `SyntaxSection` composable. - Add `SectionsList` composable. - Add `PitfallsSection` composable. - Add `RelatedTopicsSection` composable.

---
+++ composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/ui/screens/detail/components/IntroductionSection.kt
@@ -0,0 +1,20 @@
+package com.developersbreach.kotlindictionarymultiplatform.ui.screens.detail.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.developersbreach.kotlindictionarymultiplatform.data.detail.model.KotlinTopicDetails
+import kotlindictionarymultiplatform.composeapp.generated.resources.Res
+import kotlindictionarymultiplatform.composeapp.generated.resources.introduction
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun IntroductionSection(topic: KotlinTopicDetails) {
+    Text(stringResource(Res.string.introduction), style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.onPrimary)
+    Spacer(modifier = Modifier.height(4.dp))
+    Text(text = topic.intro, style = MaterialTheme.typography.bodyMedium)
+}
---
+++ composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/ui/screens/detail/components/RelatedTopicsSection.kt
@@ -0,0 +1,25 @@
+package com.developersbreach.kotlindictionarymultiplatform.ui.screens.detail.components
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.developersbreach.kotlindictionarymultiplatform.data.detail.model.KotlinTopicDetails
+import kotlindictionarymultiplatform.composeapp.generated.resources.Res
+import kotlindictionarymultiplatform.composeapp.generated.resources.bullet_item
+import kotlindictionarymultiplatform.composeapp.generated.resources.related_topics
+import org.jetbrains.compose.resources.stringResource
+
+@Composable
+fun RelatedTopicsSection(topic: KotlinTopicDetails) {
+    if (topic.relatedTopics.isNotEmpty()) {
+        Text(stringResource(Res.string.related_topics), style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.onPrimary)
+        Spacer(Modifier.height(4.dp))
+        topic.relatedTopics.forEach {
+            Text(stringResource(Res.string.bullet_item, it), style = MaterialTheme.typography.bodyMedium)
+        }
+    }
+}
