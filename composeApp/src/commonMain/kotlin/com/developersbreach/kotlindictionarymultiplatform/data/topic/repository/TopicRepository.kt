bd85fab9ba13e821f09d1d33bc73d92137edbc7e
Shreyas Deshmukh <shreyesdeshmukh03@gmail.com>
2025-06-18 23:40:02 +0530

    Refactor `TopicRepository` to use constructor injection for `HttpClient`.

--- composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/data/topic/repository/TopicRepository.kt
+++ composeApp/src/commonMain/kotlin/com/developersbreach/kotlindictionarymultiplatform/data/topic/repository/TopicRepository.kt
@@ -8,31 +8,14 @@ import com.developersbreach.kotlindictionarymultiplatform.core.network.topicSour
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.serialization.json.Json
-
-//object TopicRepository {
-//
-//    private val client = HttpClient {
-//        install(ContentNegotiation) {
-//            json(Json { ignoreUnknownKeys = true })
-//        }
-//    }
-//
-//    suspend fun getTopics(): Either<Throwable, List<Topic>> {
-//        return Either.catch {
-//            val response: Response = client.get(FirestoreConstants.TOPICS_URL).body()
-//            response.topics.map { it.toTopic() }
-//        }
-//    }
-//}
 
 class TopicRepository(
-    private val httpClient: HttpClient
-){
-    suspend fun getTopics(): Either<Throwable, List<Topic>> = Either.catch {
+    private val httpClient: HttpClient,
+) {
+    suspend fun getTopics(): Either<Throwable, List<Topic>> {
+        return Either.catch {
             val response: Response = httpClient.get(FirestoreConstants.TOPICS_URL).body()
             response.topics.map { it.toTopic() }
+        }
     }
 }
